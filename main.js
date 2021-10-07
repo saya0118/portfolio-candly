d98f485c6f0c2cd2076e266b51b4fa8e1bf91ca5
saya0118 <chemi0802@gmail.com>
2021-10-06 19:01:46 -0700

    modified js file

--- main.js
+++ main.js
@@ -27,69 +27,77 @@ function clickEvent(){
         if(event.target.parentElement.classList.contains('add')){
             
             
-            let fullPath =
-            event.target.parentElement.firstElementChildren; //removed '.src'
-            // let pos = fullPath.indexOf('image')+5;
-            // let partPath = fullPath.slice(pos);
+            let fullPath = event.target.parentElement.parentElement.firstElementChild.src;
+            let pos = fullPath.indexOf('image')+5;
+            let partPath = fullPath.slice(pos);
 
-            console.log(fullPath);
-        }
-                });
-            });  
-        })();
+            // console.log(fullPath);
 
-//             const item = {};
-//             item.img = `shopping-img${partPath}`;
+            const item = {};
+            item.img = `image${partPath}`;
             
-//             let name = event.target.parentElement.children[1].textContent;
-//             item.name = name;
+            let name = event.target.parentElement.parentElement.children[1].textContent;
+            item.name = name;
             
-//             let price = event.target.parentElement.nextElementChild.children[0].children[1].textContent;
+            let price = event.target.parentElement.parentElement.children[2].children[0].textContent;
 
-//             let finalPrice = price.slice(1).trim();
-//             item.price = finalPrice;
+            // let finalPrice = price.slice(1).trim();
+            item.price = price;
 
-//             console.log(finalPrice)
+            console.log(price)
 
-//             console.log(name);
+            console.log(name);
 
-//             console.log(item);
+            console.log(item);
 
-//             const cartItem = document.createElement('div');
-//             cartItem.classList('cart');
+            const cartList = document.createElement('li');
+            cartList.classList.add('item');
 
-//             cartItem.innerHTML = "${item.img}"
+            cartList.innerHTML = `
+                    <div class="image">
+                        <img id="item-img" src="${item.img}" alt="">
+                    </div>
+                    <div class="description">
+                        <span>${item.name}</span><br>
+                        <span>$<span id="item-price">${item.price}</span></span>
+                    </div>
+                    <div class="delete"> 
+                        <button class="delete-button" type="button" name="button">
+                            <img src="image/DeleteFilled.png" alt="">
+                        </button>
+                    </div>
+            `;
 
-//             //select cart
-//             const cart = document.getElementById('cart');
-//             const total = document.querySelector('.total-price');
-
-//             cart.insertBefore(cartItem, total);
-
+            //select cart
+            const cart = document.getElementById('cart');
+            const total = document.querySelector('.total-price');
+            cart.insertBefore(cartList, total);
+            showTotals();
             
-//             }
-//         });
-//     });  
+            }
+        });
+    });  
 
-// //show totals
+//show totals
 
-//     function showTotals(){
-//     const total = [];
-//     const items = document.querySelectorAll('#collection-price');
+    function showTotals(){
+    const total = [];
+    const items = document.getElementById('collection-price');
 
-//     items.forEach(function(item){
-//         total.push(parseFloat(item.textContent));
-//     });
-//     // console.log(total);
+    items.forEach(function(item){
+        total.push(parseFloat(item.textContent));
+    });
+    // console.log(total);
 
-//     const totalPrice = total.reduce(function(total,item){
-//         total += item;
-//         return total;
-//     },0);
-//     const finalPrice = totalPrice.toFixed(2);
+    const totalPrice = total.reduce(function(total,item){
+        total += item;
+        return total;
+    },0);
+    const finalPrice = totalPrice.toFixed(2);
+    // console.log(finalPrice);
 
-//     document.getElementById('total-price').textContent = finalPrice;
-//     document.getElementById('nav-cart-total').textContent = finalPrice;
-//     document.getElementById('item-count').textContent = total.length;
-// }
-// })();
+    document.getElementById('total-price').textContent = finalPrice;
+    document.getElementById('nav-cart-total').textContent = finalPrice;
+    document.getElementById('item-count').textContent = total.length;
+}
+})();
